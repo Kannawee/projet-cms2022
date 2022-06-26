53bbaf3d5e42dda9be81afa9fa2e53dc24f5cef0
Samuel GUENIER <drakelyx@gmail.com>
2022-06-26 16:49:49 +0200

    better files organization of QueryBuilder + testController

---
+++ www/Controller/Testsam.class.php
@@ -0,0 +1,25 @@
+<?php
+
+    namespace App\Controller;
+
+    use App\Core\MysqlBuilder as MysqlBuilder;
+
+    class TestSam extends MysqlBuilder
+    {
+
+        public function test()
+        {
+
+            $col = ["col1", "col2", "col3"];
+            $this->init();
+            $this->select('MaTable', $col);
+            $this->where("col1", "val1");
+            $this->where("col2", "val2");
+            $this->limit(0, 1);
+
+            echo '<pre>'.$this->getQuery();
+        }
+
+
+
+    }
