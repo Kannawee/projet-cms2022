b32a10b7821e06e1446c01aa1ee64f2b87f18b51
Samuel GUENIER <drakelyx@gmail.com>
2022-07-09 12:12:52 +0200

    fix mail + SQLBuilder + modif Sql.class

--- www/Core/Sql.class.php
+++ www/Core/Sql.class.php
@@ -1,78 +1,102 @@
 <?php
 
-namespace App\Core;
+    namespace App\Core;
 
-abstract class Sql
-{
-    private $pdo;
-    private $table;
+    use App\Core\MysqlBuilder as MysqlBuilder;
 
-    public function __construct()
+    abstract class Sql
     {
-        //Se connecter à la bdd
-        //il faudra mettre en place le singleton
-        try{
-            $this->pdo = new \PDO( DBDRIVER.":host=".DBHOST.";port=".DBPORT.";dbname=".DBNAME
-                ,DBUSER, DBPWD , [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_WARNING]);
-        }catch (\Exception $e){
-            die("Erreur SQL : ".$e->getMessage());
-        }
+        private $pdo;
+        private $table;
+        private $builder;
 
-        //Si l'id n'est pas null alors on fait un update sinon on fait un insert
-        $calledClassExploded = explode("\\",get_called_class());
-        $this->table = strtolower(DBPREFIXE.end($calledClassExploded));
-
-    }
-
-    /**
-     * @param int $id
-     */
-    public function checkId(?int $id): object
-    {
-        $sql = "SELECT * FROM ".$this->table." WHERE id=".$id;
-        $query = $this->pdo->query($sql);
-        return $query->fetchObject(get_called_class());
-    }
-
-    public function save()
-    {
-
-        $columns = get_object_vars($this);
-        $columns = array_diff_key($columns, get_class_vars(get_class()));
-
-        if($this->getId() == null){
-            $sql = "INSERT INTO ".$this->table." (".implode(",",array_keys($columns)).") 
-            VALUES ( :".implode(",:",array_keys($columns)).")";
-        }else{
-            $update = [];
-            foreach ($columns as $column=>$value)
-            {
-                $update[] = $column."=:".$column;
+        public function __construct()
+        {
+            //Se connecter à la bdd
+            //il faudra mettre en place le singleton
+            try{
+                $this->pdo = new \PDO( DBDRIVER.":host=".DBHOST.";port=".DBPORT.";dbname=".DBNAME
+                    ,DBUSER, DBPWD , [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_WARNING]);
+            }catch (\Exception $e){
+                die("Erreur SQL : ".$e->getMessage());
             }
-            $sql = "UPDATE ".$this->table." SET ".implode(",",$update)." WHERE id=".$this->getId() ;
-        }
-        //echo $sql;
-        //die();
-        $queryPrepared = $this->pdo->prepare($sql);
-        var_dump($queryPrepared);
-        $queryPrepared->execute( $columns );
 
-    }
+            //Si l'id n'est pas null alors on fait un update sinon on fait un insert
+            $calledClassExploded = explode("\\",get_called_class());
+            $this->table = strtolower(DBPREFIXE.end($calledClassExploded));
+            $this->builder = new MysqlBuilder();
+            $this->builder->init();
 
-    public function select($where, $limit)
-    {
-        $sql = "SELECT * FROM ".$this->table." WHERE ";
-
-        foreach ($where as $col => $val) {
-            $sql .= $col."='".$val."' AND ";
         }
 
-        $sql = trim($sql, " AND ");
-        $sql .= " LIMIT ".$limit;
+        /**
+         * @param int $id
+         */
+        public function checkId(?int $id): object
+        {
+            $sql = "SELECT * FROM ".$this->table." WHERE id=".$id;
+            $query = $this->pdo->query($sql);
+            return $query->fetchObject(get_called_class());
+        }
 
-        return $this->pdo->query($sql)->fetch();
+        public function save()
+        {
+
+            $columns = get_object_vars($this);
+            $columns = array_diff_key($columns, get_class_vars(get_class()));
+
+            if($this->getId() == null){
+                $sql = "INSERT INTO ".$this->table." (".implode(",",array_keys($columns)).") 
+                VALUES ( :".implode(",:",array_keys($columns)).")";
+            }else{
+                $update = [];
+                foreach ($columns as $column=>$value)
+                {
+                    $update[] = $column."=:".$column;
+                }
+                $sql = "UPDATE ".$this->table." SET ".implode(",",$update)." WHERE id=".$this->getId() ;
+            }
+            //echo $sql;
+            //die();
+            $queryPrepared = $this->pdo->prepare($sql);
+            var_dump($queryPrepared);
+            $queryPrepared->execute( $columns );
+
+        }
+
+        public function select($where, $limit=null)
+        {
+            $this->builder->select($this->table, ["*"]);
+
+            foreach ($where as $col => $val) {
+                $this->builder->where($col, $this->table);
+            }
+
+            if (!is_null($limit)) {
+                $this->builder->limit($limit);
+            }
+    
+            $sql = $this->builder->getQuery();
+            
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute($where);
+            return $stmt->fetchAll($this->pdo::FETCH_ASSOC);
+        }
+
+        public function delete($where)
+        {
+            $this->builder->delete($this->table);
+
+            foreach ($where as $col=>$val) {
+                $this->builder->where($col, $this->table);
+            }
+
+            $sql = $this->builder->getQuery();
+
+            $stmt = $this->pdo->prepare($sql);
+            return $stmt->execute($where);
+
+        }
+
 
     }
-
-
-}
