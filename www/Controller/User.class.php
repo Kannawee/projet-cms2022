1e7cb323258d0b1cc8640889584c1392611e2aca
agraval <alexandre.samson78@gmail.com>
2022-07-09 12:31:22 +0200

    wip token user

--- www/Controller/User.class.php
+++ www/Controller/User.class.php
@@ -5,17 +5,39 @@ use App\Core\CleanWords;
 use App\Core\Sql;
 use App\Core\Verificator;
 use App\Core\View;
+use App\Core\MysqlBuilder as MysqlBuilder;
 use App\Model\User as UserModel;
 
 class User {
 
     public function login()
     {
-        $view = new View("Login", "back");
+        $user = new UserModel();
+        if (!empty($_POST)) {
+            $result = Verificator::checkForm($user->getLoginForm(), $_POST);
+            /* SI RESULT != EMPTY -> REDIRECT /LOGIN */
+            $user->setEmail($_POST['email']);
+            $user->setPassword($_POST['password']);
+            $res = $user->checkLogin()[0];
+            if ($user->getEmail() === $res['email'] && password_verify($_POST['password'], $res['password'])) {
+                $_SESSION["idUser"] = $res['id'];
+                $token = $user->generateToken();
+                $_SESSION['token'] =$token;
+                var_dump($_SESSION);
+                if ($res['status'] == "1") {
+                    $_SESSION['isAdmin'] = 1;
+                    header("Location: /administration/projects");
+                }
+                $view = new View("Home", "front");
+                $view->assign("user", $user);
+                $view->assign("firstname", 'test');
+            }
+        } else {
+            // AJOUTER LA REDIRECTION SI USER!=ADMIN-> HOME PAGE
+            $view = new View("Login", "front" );
+            $view->assign("user", $user);
+        }
 
-        $view->assign("pseudo", "Prof");
-        $view->assign("firstname", "Yves");
-        $view->assign("lastname", "Skrzypczyk");
 
     }
 
@@ -28,7 +50,6 @@ class User {
         if( !empty($_POST)){
 
             $result = Verificator::checkForm($user->getRegisterForm(), $_POST);
-            print_r($result);
             $user->setEmail($_POST['email']);
             $user->setLogin($_POST['login']);
             $user->setPassword($_POST['password']);
@@ -43,7 +64,8 @@ class User {
 
     public function logout()
     {
-        echo "Se déco";
+        session_destroy();
+        header("location: /");
     }
 
 
@@ -53,7 +75,6 @@ class User {
     }
 
 }
-
 
 
 
