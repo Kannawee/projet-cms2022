5d1583930f79b21bf92716a93080f81f2c626ae5
Samuel GUENIER <drakelyx@gmail.com>
2022-07-09 14:16:16 +0200

    wip Testsam Controller + Sql.class + Mymail.class

--- www/Controller/Testsam.class.php
+++ www/Controller/Testsam.class.php
@@ -14,10 +14,13 @@
             $mail = new MymailModel();
             
             $addresse = array(
-                "Samuel GUENIER" => "guenier.samuel@gmail.com"
+                "Samuel GUENIER" => "guenier.samuel@gmail.com",
+                "Izia" => "izia.crinier@gmail.com",
+                "Alex" => "alexandre.samson78@gmail.com"
+
             );
     
-            $mail->setupMyMail("Test Sujet", "Test Body", $addresse);
+            $mail->setupMyMail("ARTISTERY BOOMBOOM", "WOUHOUUUUUUU CA MARCHE", $addresse);
     
             $response = $mail->sendMyMail();
     
