e3126873bd2095759f4b82348b8ac9567d5a92d7
Samuel GUENIER <drakelyx@gmail.com>
2022-07-09 13:42:41 +0200

    resolving conflict on view back

--- www/View/back.tpl.php
+++ www/View/back.tpl.php
@@ -3,21 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>Administration du site</title>
-<<<<<<< HEAD
-=======
     <link href="../style.css" rel="stylesheet" />
->>>>>>> 19752dae2c190f36969ac2558fbc7ba5b7380d00
     <meta name="description" content="Description de ma page">
 </head>
 <body>
 
-<<<<<<< HEAD
-<!--    <?php include "View/".$this->view.".view.php"; ?>-->
-    <p>TEMPLATE BACK</p>
-    <p>View Dashboard</p>
-=======
    <?php include "View/".$this->view.".view.php"; ?>
->>>>>>> 19752dae2c190f36969ac2558fbc7ba5b7380d00
 
 </body>
 </html>
