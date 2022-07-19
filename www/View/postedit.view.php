cb8a6f62e810433ce1a64a939b0514a16f4e303b
Samuel <drakelyx@gmail.com>
2022-07-19 14:12:51 +0200

    modif on postedit view

--- www/View/postedit.view.php
+++ www/View/postedit.view.php
@@ -1,3 +1,11 @@
+<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+
+<script>
+    tinymce.init({
+        selector: '#contentPost'
+    });
+</script>
+
 <!-- style a faire -->
 <h1>Post edit</h1>
 <!-- modif : lien retour vers liste -->
